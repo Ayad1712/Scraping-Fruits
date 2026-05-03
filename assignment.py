5c3b68f5b0613ce356e847568b8e13025c4d47dc
Ayad1712 <ayadhaider06@gmail.com>
2026-05-03 11:56:15 -0400

    assignment.py

---
+++ assignment.py
@@ -0,0 +1,17 @@
+#Assignment
+assignment_url = "https://faruk-hasan.com/ai_resources/missing_data.html"
+get_req = requests.get(assignment_url) #visit url
+parse = BeautifulSoup(get_req.text, 'html.parser') #parse it
+fruit_name_list = parse.find_all("div", class_="fruit")
+fruits_set = set()
+for i in fruit_name_list:
+  clean_fruits = i.text.strip().lower()
+  fruits_set.add(clean_fruits)
+fruit_array = np.array(list(fruits_set),dtype=str)
+for i in range(len(fruit_array)):
+  if fruit_array[i] in ["", "null", "nan"]:
+    fruit_array[i] = ""
+for i in fruit_array:
+  if i != "":
+    var = i.capitalize()
+    print(var)
